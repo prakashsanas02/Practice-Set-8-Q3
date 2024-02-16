29859c251e68139cf889f1cc9e416cda2b91edd7
prakashsanas02 <156497321+prakashsanas02@users.noreply.github.com>
2024-02-16 16:05:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("google").addEventListener("click", function() { 
+window.location = "https://www.google.com";
+win.focus();
+})   
+
+document.getElementById("facebook").addEventListener("click", function() { 
+window.location = "https://www.facebook.com";
+win.focus();
+})  
+
+document.getElementById("twitter").addEventListener("click", function() { 
+window.location = "https://www.twitter.com";
+win.focus();
+})  
+
+document.getElementById("instagram").addEventListener("click", function() { 
+window.location = "https://www.instagram.com";
+win.focus();
+})  
